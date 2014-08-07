2d3369c7a78493e276295053dab1b4ac42d23834
Noah Halford <noah.halford@gmail.com>
2014-08-06 23:06:33 -0500

    Caffio-test completed.

---
+++ Caffio-test/CardView.swift
@@ -0,0 +1,27 @@
+//
+//  CardView.swift
+//  Caffio-test
+//
+//  Created by Noah Halford on 8/4/14.
+//  Copyright (c) 2014 Noah Halford. All rights reserved.
+//
+
+import UIKit
+
+class CardView: UIView {
+
+    init(frame: CGRect) {
+        super.init(frame: frame)
+        // Initialization code
+    }
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect)
+    {
+        // Drawing code
+    }
+    */
+
+}
